f810d951f73743d49464ebc4ed1e961f6695d3b5
piyush goyal <piyush2022ug@gmail.com>
2025-05-19 11:26:45 +0530

    refactor: remove db connection module and streamline MongoDB connection in server.js

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
